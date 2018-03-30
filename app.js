104013e5619bd45b2cc537cc506406070b1f749b
CannonFodderr <izhaki.idan@gmail.com>
2018-03-30 15:33:41 +0300

    Add custom css & semantic-ui css to header partial

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(dbUrl);
 // ===================
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "public"));
+app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
 // ===================
